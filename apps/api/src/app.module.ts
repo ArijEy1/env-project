3807afeabc6edf4bb0601093034c2ee00563e4e8
Parsath <bilel.taktak25@gmail.com>
2026-06-08 18:33:14 +0100

    Add admin module and register in app

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AdminModule } from './admin/admin.module';
 import { AssessmentModule } from './assessment/assessment.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule, AssessmentModule],
+  imports: [AuthModule, AssessmentModule, AdminModule],
 })
 export class AppModule {}
